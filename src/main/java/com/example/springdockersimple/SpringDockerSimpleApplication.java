00c502d80d0b34716bec8e7dd5d0462e18aec46a
Evgeniy <e.dmitrenko@yahoo.com>
2023-02-09 15:41:17 +0300

    first commit

---
+++ src/main/java/com/example/springdockersimple/SpringDockerSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdockersimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdockersimple/SpringDockerSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdockersimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
