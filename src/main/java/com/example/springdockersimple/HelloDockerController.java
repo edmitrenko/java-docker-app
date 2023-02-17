e3480dfc41adb6f2e0a1e3e0ebb78d030e313adf
Evgeniy <e.dmitrenko@yahoo.com>
2023-02-18 02:01:19 +0300

    change

--- src/main/java/com/example/springdockersimple/HelloDockerController.java
+++ src/main/java/com/example/springdockersimple/HelloDockerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController  
 public class HelloDockerController {  
   
-    @GetMapping("/hello")  
+    @GetMapping("/")  
     public String hello() {  
         return "Hello Docker v1.0!";  
     }  
