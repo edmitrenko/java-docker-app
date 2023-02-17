fc46864b1ee0fd9611cb7279c67444e452efd95e
Evgeniy <e.dmitrenko@yahoo.com>
2023-02-18 02:12:28 +0300

    change

--- src/main/java/com/example/springdockersimple/HelloDockerController.java
+++ src/main/java/com/example/springdockersimple/HelloDockerController.java
@@ -8,6 +8,6 @@ public class HelloDockerController {
   
     @GetMapping("/hello")  
     public String hello() {  
-        return "Hello Docker v1.0!";  
+        return "Hello Docker v2.0!";  
     }  
 }  
